09825624437b2c82f9ab4da2caea5148e5aa4cd3
Samir Nunes da Silva <samir.silva12342@gmail.com>
2024-07-26 23:36:56 -0300

    add k_means.py

---
+++ k_means/src/k_means.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+
+class KMeans:
+    def __init__(self, k, max_iter=100, random_state=0):
+        self.__k = k
+        self.__max_iter = max_iter
+        self.__random_state = random_state
+        self.centroids = None
+        self.clusters = None
+    
+    def fit(self, X: pd.DataFrame):
+        assert self.__k <= len(X)
+        centroids = self.__init_centroids(X)
+        clusters = self.__assign_clusters(X, centroids)
+        iter_count = 0
+        while True:
+            new_centroids = self.__update_centroids(X, clusters)
+            new_clusters = self.__assign_clusters(X, new_centroids)
+            iter_count += 1
+            if new_centroids.equals(centroids) or iter_count >= self.__max_iter:
+                self.centroids = new_centroids
+                self.clusters = new_clusters
+                return self
+            centroids = new_centroids
+            clusters = new_clusters
+    
+    def predict(self, X: pd.DataFrame):
+        return self.__assign_clusters(X, self.centroids)
+    
+    def __init_centroids(self, X):
+        rand = np.random.RandomState(self.__random_state)
+        centroids = X.loc[rand.choice(X.index, size=self.__k, replace=False)].reset_index(drop=True).copy()
+        return centroids
+
+    def __update_centroids(self, X: pd.DataFrame, clusters: pd.Series):
+        X_with_clusters = X.copy()
+        X_with_clusters["cluster"] = clusters
+        centroids = X_with_clusters.groupby(by="cluster").mean()
+        return centroids
+    
+    def __assign_clusters(self, X: pd.DataFrame, centroids: pd.DataFrame):
+        clusters = np.empty(X.shape[0], dtype=int)
+        for i, point in enumerate(X.values):
+            distances = np.linalg.norm(centroids.values - point, axis=1)
+            cluster = np.argmin(distances)
+            clusters[i] = cluster
+        return pd.Series(clusters, index=X.index)
